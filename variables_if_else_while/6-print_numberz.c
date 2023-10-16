b356a676f52b090706271ae6caf762eeb6a0121d
root <Part4@github.com>
2023-10-16 06:04:49 -0700

    tenz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar('0' + number);
+		number++;
+	}
+
+	putchar('\n');
+	return (0);
+}
