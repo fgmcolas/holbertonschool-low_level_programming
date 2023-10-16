ece26e85295f26e1fcf94f786c96ea32bd03e5ae
root <Part4@github.com>
2023-10-16 10:25:24 -0700

    hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int hexa = 0;
+
+	while (hexa < 16)
+	{
+	if (hexa < 10)
+	{
+		putchar('0' + hexa );
+	}
+	else
+	{
+		putchar('a' + hexa - 10);
+	}
+	hexa++;
+	}
+
+	putchar('\n');
+	return (0);
+}
