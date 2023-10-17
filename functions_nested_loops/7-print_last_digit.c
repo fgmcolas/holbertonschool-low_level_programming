804d5bd004c6079ef4324d5c746e510a02821030
root <Part4@github.com>
2023-10-17 11:06:58 -0700

    lastdig

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of n
+ * @n: The number to print
+ * main - Entry point
+ * Return: Always last_digit (Sucess)
+ * ld - lastdigit
+ * int - ld
+ */
+
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = n % 10;
+
+	putchar('0' + ld);
+	return (ld);
+}
