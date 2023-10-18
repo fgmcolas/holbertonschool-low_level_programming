7076b7745899edd07d0fb78c7837455e3a46b92c
root <Part4@github.com>
2023-10-18 08:52:42 -0700

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - see if the character is alphabetic
+ * @c: The character to print
+ * main - Entry point
+ * Return: Always 0 (Sucess)
+ *
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+	return (c);
+}
