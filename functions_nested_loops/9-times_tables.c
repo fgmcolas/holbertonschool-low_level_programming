ee1a6e73a61a335e39617f55f14f3a03797fc016
root <Part4@github.com>
2023-10-18 05:30:11 -0700

    table

---
+++ functions_nested_loops/9-times_tables.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - multiplication table
+ *
+ * unit - the unit who will be multiplicated
+ *
+ * multi - the multiplicator
+ *
+ * result - the result of the multiplication
+ */
+
+void times_table(void)
+{
+	int unit;
+	int multi;
+	int result;
+
+	for (unit = 0; unit < 10; unit++)
+	{
+		for (multi = 0; multi < 10; multi++)
+		{
+			result = unit * multi;
+
+				if (multi < 9)
+				{
+					if (result < 10)
+					{
+						_putchar(' ');
+					}
+					else
+					{
+						_putchar('0' + result / 10);
+					}
+					_putchar('0' + result % 10);
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('0' + result / 10);
+					_putchar('0' + result % 10);
+					_putchar('\n');
+				}
+		}
+	}
+}
