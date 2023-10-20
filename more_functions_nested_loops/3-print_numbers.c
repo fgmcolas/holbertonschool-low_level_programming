5b720e0ac8c302551009108bd76710ab4fa00a7c
root <Part4@github.com>
2023-10-20 03:20:15 -0700

    addprintnumbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prototype for main.h
+ *
+ * @c: Character
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
