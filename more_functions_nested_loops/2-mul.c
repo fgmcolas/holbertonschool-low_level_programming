6094af38f7a212f173c0a5d4adbc197b11ee3d42
root <Part4@github.com>
2023-10-20 02:58:10 -0700

    addmul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - prototype for main.h
+ *
+ * @a: Character st
+ * @b: Character nd
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
