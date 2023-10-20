30158a433cfc296aa837d92801116f50407e1e8c
root <Part4@github.com>
2023-10-20 03:55:36 -0700

    addmorenumbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prototype for main.h
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		int n;
+	for (n = 0; n <= 14; n++)
+	{
+		if (n < 10)
+		{
+		_putchar('0' + n);
+		}
+		else
+		{
+			_putchar('0' + n / 10);
+			_putchar('0' + n % 10);
+		}
+	}
+	_putchar('\n');
+	}
+}
