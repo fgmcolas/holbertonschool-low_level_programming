0942cd6b6af823a951410fcfad3ae75dbbd36321
root <Part4@github.com>
2023-10-23 12:44:26 -0700

    addputs2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0' && str[i + 1] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
+		if (i < '\0')
 		_putchar(str[i]);
 	}
 	_putchar('\n');
