cc96f15cfc503115d8d89a188a96dd1b981f91b0
root <Part4@github.com>
2023-10-24 02:32:29 -0700

    addstrpcy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - prototypes
+ *
+ * @dest: pointer
+ * @src: pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
