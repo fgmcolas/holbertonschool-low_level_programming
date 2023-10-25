02f33380bbf183c654b05816badde1629649e76c
root <Part4@github.com>
2023-10-25 06:28:01 -0700

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - prototypes
+ *
+ * @dest: pointer to destination
+ * @src: pointer to source
+ * @n: number
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	while (i < n)
+		dest[i++] = '\0';
+	return (dest);
+}
