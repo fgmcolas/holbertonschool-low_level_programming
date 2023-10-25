a1d52b2617c4857622e4c7fed72170874281fd37
root <Part4@github.com>
2023-10-25 03:03:23 -0700

    addatoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - prototypes
+ *
+ * @s: pointer
+ *
+ * Return: sign*result
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int a = 0;
+
+	
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+			i++;
+		}
+
+		else if (s[i] == '+')
+		{
+			i++;
+		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+		result = result * 10 + (s[i] - '0');
+		a = 1;
+		i++;
+		}
+	
+		else if (a == 1)
+		{
+			break;
+		}
+	}
+	result *= sign;
+	return (result);
+}
