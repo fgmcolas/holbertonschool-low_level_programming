b2ee6de28a105fe1e74a99b55535a167643aba6f
root <Part4@github.com>
2023-10-26 14:30:34 -0700

    addcapstring

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - prototypes
+ *
+ * @s: pointer
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+		{
+			i++;
+		}
+		if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' || i == 0)
+		{
+			s[i] = s[i] - 32;
+			i++;
+		}
+	}
+	return (s);
+}
