537053e0aaee4d2a9eff682369cb6ee14e718144
root <Part4@github.com>
2023-10-28 07:39:54 -0700

    addstrstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - prototypes
+ *
+ * @haystack: pointer
+ * @needle: pointer
+ *
+ * Return: NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+		{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
