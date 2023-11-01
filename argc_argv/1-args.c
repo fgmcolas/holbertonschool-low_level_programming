6669a9a06af0181275a742e1afa708c9837cdd3b
root <Part4@github.com>
2023-11-01 04:27:56 -0700

    addargs

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,13 +5,12 @@
  *
  * @argc: variable
  * @argv: char
- *
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	(void)argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
