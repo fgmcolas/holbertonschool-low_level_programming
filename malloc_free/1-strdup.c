96c083388643617dcf073c850136810b3611a385
root <Part4@github.com>
2023-11-06 11:40:34 -0800

    addstrdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Prototypes
+ *
+ * @str: char
+ *
+ * Return: NULL or p
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i = 0;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	while (str[i])
+	{
+		i++;
+	}
+
+	p = (char *) malloc(sizeof(char) * (i + 1));
+
+	for (i = 0; str[i]; i++)
+	{
+		p[i] = str[i];
+	}
+	p[i] = '\0';
+	return (p);
+}
