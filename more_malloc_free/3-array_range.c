f96957016bac3e1a574665eb3b6cfac5848532d2
root <Part4@github.com>
2023-11-08 08:10:39 -0800

    addarrayrange

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * array_range - Pototypes
+ *
+ * @min: int
+ * @max: int
+ *
+ * Return: NULL or a
+ */
+
+int *array_range(int min, int max)
+{
+	int d;
+	int i;
+	int *a;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	d = max - min;
+	a = (int *) malloc(sizeof(int) * (d + 1));
+	
+	if (a == (NULL))
+	{
+		return (NULL);
+	}
+
+	for (i = 0; min < max; i++)
+	{
+		a[i] = min;
+		min++;
+	}
+	return (a);
+}
