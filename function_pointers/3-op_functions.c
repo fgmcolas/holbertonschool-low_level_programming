1f2399b32c05da32208b1c24aad579426b74b589
root <Part4@github.com>
2023-11-15 04:58:07 -0800

    addall3-exercices

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function
+ * @a: int a
+ * @b: int b
+ * Return: operation of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function
+ * @a: int a
+ * @b: int b
+ * Return: operation of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function
+ * @a: int a
+ * @b: int b
+ * Return: operation of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function
+ * @a: int a
+ * @b: int b
+ * Return: operation of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function
+ * @a: int a
+ * @b: int b
+ * Return: operation of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
