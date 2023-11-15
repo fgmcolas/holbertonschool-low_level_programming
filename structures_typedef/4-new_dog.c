d66e822a72bd22af15b397c22790a28b337d9f63
root <Part4@github.com>
2023-11-15 02:45:11 -0800

    addnewdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,19 +19,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	dog->name = malloc(strlen(name) +1);
+	dog->name = strdup(name);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(strlen(owner) +1);
+	dog->age = age;
+	dog->owner = strdup(owner);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	dog->age = age;
 	return (dog);
 }
