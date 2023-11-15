9abe587cd7f650c1d8917caaf4860ef3d1bcc399
root <Part4@github.com>
2023-11-15 05:27:57 -0800

    edit 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,8 +11,8 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b;
-	char *o;
+	int num1, num2;
+	char *operator;
 
 	if (argc < 4)
 	{
@@ -20,22 +20,22 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	a = atoi(argv[1]);
-	o = argv[2];
-	b = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
 
-	if (get_op_func(o) == NULL || o[1] != '\0')
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*o == '/' && b == 0) || (*o == '%' && b == 0))
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(o)(a, b));
+	printf("%d\n", get_op_func(operator)(num1, num2));
 	return (0);
 }
