25a0d69c57e3326b6cc5eac43cedf97d9d5cf150
root <Part4@github.com>
2023-11-15 03:50:02 -0800

    addarrayiterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t t;
 
-	if (array == 0 || size == 0 || array == 0)
+	if (array == 0 || size == 0 || action == 0)
 		return;
 	for (t = 0 ; t < size; t++)
 		action(array[t]);
