627adef4d1369281f78c4d9944b512624eeb7035
root <Part4@github.com>
2023-11-29 14:41:28 -0800

    edit the 7-insert_dnodeint.c file

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	for (i = 0; i < idx - 1 && temp != NULL; i++)
 	{
-		if (temp == NULL || temp->next == NULL)
+		if (temp->next == NULL)
 			return (NULL);
 
 		temp = temp->next;
