db4733b1dbe250af72b57f1cb4f32b6a64d489f7
root <Part4@github.com>
2023-12-06 11:34:51 -0800

    delete lines for betty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -18,9 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	current = ht->array[index];
-
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
@@ -33,11 +31,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 	}
-
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
-
 	newNode->key = strdup(key);
 	newNode->value = strdup(value);
 	if (newNode->key == NULL || newNode->value == NULL)
@@ -47,9 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(newNode);
 		return (0);
 	}
-
 	newNode->next = ht->array[index];
 	ht->array[index] = newNode;
-
 	return (1);
 }
