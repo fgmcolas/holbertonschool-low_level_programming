000d0cf28e6a1722432912cb2100b62f299aee3b
root <Part4@github.com>
2023-12-06 05:25:03 -0800

    adding the exercice 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create a hash table
+ * @size: The size of the array
+ * Return: A pointer to the hash table or NULL if error
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+
+	if (!ht)
+		return (NULL);
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (!ht->array)
+		return (NULL);
+
+	ht->size = size;
+
+	while (size--)
+		ht->array[size] = NULL;
+
+	return (ht);
+}
