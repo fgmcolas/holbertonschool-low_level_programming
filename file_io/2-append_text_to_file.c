1a9e738b1e619d300ec164b8e828a7b1b7fac242
root <Part4@github.com>
2023-12-05 16:34:29 -0800

    editing the file 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-i
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
