23408d860d78fd931d7131c8561a9b60c755a6e0
root <Part4@github.com>
2023-12-06 05:53:11 -0800

    adding unsigned long int and switch size-- by size ++

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,22 +10,23 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	hash_node_t **array;
+	unsigned long int uli;
 
 	ht = malloc(sizeof(hash_table_t));
 
-	if (!ht)
+	if (ht == NULL)
 		return (NULL);
 
 	array = malloc(sizeof(hash_node_t *) * size);
 
-	if (!array)
+	if (array == NULL)
 		return (NULL);
+
+	for (uli = 0; uli < size; uli++)
+		array[uli] = NULL;
 
 	ht->array = array;
 	ht->size = size;
-
-	while (size--)
-		ht->array[size] = NULL;
 
 	return (ht);
 }
