daa1c982f35432f12ab0a24563f3b1d209e5fab7
root <Part4@github.com>
2023-12-06 11:31:43 -0800

    adding the 3-hash_table_set.c file

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: The hash table to add or update the key/value to
+ * @key: The key (cannot be an empty string)
+ * @value: The value associated with the key (must be duplicated)
+ *
+ * Return: 1 if it succeeded or 0
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *newNode, *current;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	newNode = malloc(sizeof(hash_node_t));
+	if (newNode == NULL)
+		return (0);
+
+	newNode->key = strdup(key);
+	newNode->value = strdup(value);
+	if (newNode->key == NULL || newNode->value == NULL)
+	{
+		free(newNode->key);
+		free(newNode->value);
+		free(newNode);
+		return (0);
+	}
+
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
+
+	return (1);
+}
