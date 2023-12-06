4050633b5814a7274bb8bfd978de16d4216d3f40
root <Part4@github.com>
2023-12-05 16:30:57 -0800

    editing the file 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,23 +16,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fileDescriptor = open(filename, O_WRONLY | O_APPEND);
-
-	if (fileDescriptor == -1)
-		return (-1);
-
-	for (len = 0; text_content[len] != '\0'; len++)
-		;
-
 	if (text_content != NULL)
 	{
-		bytesWritten = write(fileDescriptor, text_content, strlen(text_content));
-		if (bytesWritten == -1)
-		{
-			close(fileDescriptor);
-			return (-1);
-		}
+		for (len = 0; text_content[len]; len++)
+			;
 	}
+
+	fileDescriptor = open(filename, O_WRONLY | O_APPEND);
+	bytesWritten = write(fileDescriptor, text_content, len);
+
+	if (fileDescriptor == -1 || bytesWritten == -1)
+		return (-1);
 
 	close(fileDescriptor);
 	return (1);
