19175d4a30bafe2c92304dbfdc2ce47c6ec2926f
root <Part4@github.com>
2023-12-06 06:11:47 -0800

    check if the checker fail for ex 0 is betty

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,12 +5,31 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * struct hash_node - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the hashtable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the list
+ */
+
 typedef struct hash_node
 {
 	char *key;
 	char *value;
 	struct hash_node *next;
 } hash_node_t;
+
+/**
+ * struct hash_table - Hash table data structure
+ *
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a chaining collision handling
+ */
 
 typedef struct hash_table
 {
