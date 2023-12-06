aab79113b6b6d1e7d8aeeca26071d7aef5e36dd1
root <Part4@github.com>
2023-12-06 05:29:05 -0800

    adding a free() line

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,7 +18,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!ht->array)
+	{
+		free(ht);
 		return (NULL);
+	}
 
 	ht->size = size;
 
